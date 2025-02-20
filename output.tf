8ea37086ca877e7fd6f9ac89c4f6a576022660e9
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-02-20 10:25:20 +0300

    inital commit

--- output.tf
+++ output.tf
@@ -1,21 +1,28 @@
 output "store_ui_url" {
   value = module.store-core.store_ui_url
+  sensitive = false
 }
 output "welcome_ui_url" {
   value = module.store-core.welcome_ui_url
+  sensitive = false
 }
 output "auth_url" {
   value = module.store-core.auth_url
+  sensitive = false
 }
 output "org1_store1_url" {
   value = "https://org1-store1.${module.store-pod["default"].domain}"
+  sensitive = false
 }
 output "org1_store2_url" {
   value = "https://org1-store2.${module.store-pod["default"].domain}"
+  sensitive = false
 }
 output "org2_store1_url" {
   value = "https://org2-store1.${module.store-pod["default"].domain}"
+  sensitive = false
 }
 output "org2_store2_url" {
   value = "https://org2-store2.${module.store-pod["default"].domain}"
+  sensitive = false
 }
