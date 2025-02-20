449545198bc5b0f727fa69b1d29a43b33c8e63f1
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-02-20 09:58:22 +0300

    inital commit

--- main.tf
+++ main.tf
@@ -4,19 +4,12 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-resource "random_string" "project" {
-  length  = 8
-  upper   = false
-  lower   = true
-  numeric = false
-  special = false
-}
+
 
 locals {
-  project              = random_string.project.result
-  store_core_namespace = "store-core.${local.project}.lcl"
+  store_core_namespace = "store-core.${var.project}.lcl"
   tags = {
-    Project     = local.project
+    Project     = var.project
     Terraform   = "true"
     Environment = var.env
   }
@@ -30,7 +23,7 @@ locals {
       name : "store-pod-${lookup(value, "index")}"
       org : lookup(value, "org")
       endpoint : lookup(value, "endpointType") == "EXTERNAL" ? lookup(value, "endpoint") :
-        "store-pod-${lookup(value, "id")}.${local.project}.lcl"
+        "store-pod-${lookup(value, "id")}.${var.project}.lcl"
       endpointType : lookup(value, "endpointType")
       size : lookup(value, "size")
     }
@@ -65,7 +58,7 @@ module "store-core" {
   domain                     = local.domain
   certificate_arn            = local.domainCertificateArn
   domain_zone_name           = data.aws_route53_zone.domain_zone.name
-  project                    = local.project
+  project                    = var.project
   tags                       = local.tags
   database_subnets           = module.vpc.database_subnets
   vpc_cidr_block             = local.vpc_cidr
@@ -88,7 +81,7 @@ module "store-pod" {
   certificate_arn      = local.domainCertificateArn
   store_core_namespace = local.store_core_namespace
   domain_zone_name     = data.aws_route53_zone.domain_zone.name
-  project              = local.project
+  project              = var.project
   tags                 = local.tags
   database_subnets     = module.vpc.database_subnets
   vpc_cidr_block       = local.vpc_cidr
