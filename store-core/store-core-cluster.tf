1c347b7d703876b9d1841d20d0ba6d529e5971d1
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-02-22 10:53:33 +0300

    inital commit

--- store-core/store-core-cluster.tf
+++ store-core/store-core-cluster.tf
@@ -66,11 +66,11 @@ locals {
     },
     {
       "name" : "SPRING_RABBITMQ_USERNAME",
-      "value" : var.mq_username
+      "value" : jsondecode(aws_secretsmanager_secret_version.mq_secret_version.secret_string)["username"]
     },
     {
       "name" : "SPRING_RABBITMQ_PASSWORD",
-      "value" : var.mq_password
+      "value" : jsondecode(aws_secretsmanager_secret_version.mq_secret_version.secret_string)["password"]
     },
   ]
   subscription_env = [
