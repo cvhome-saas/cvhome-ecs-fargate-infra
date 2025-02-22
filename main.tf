59629a82fb23d66bb4349a9fd3eb7f45e8c586a2
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-02-22 13:19:22 +0300

    inital commit

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
   private_ecr_docker_registry = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
   docker_registry             = var.docker_registry != "" ? var.docker_registry : local.private_ecr_docker_registry
   xxpods = {
-    for key in local.xpods : lookup(key,"name" ) => {
+    for value in local.xpods : lookup(value,"name" ) => {
       index : lookup(value, "index")
       id : lookup(value, "id")
       name : "store-pod-${lookup(value, "index")}"
