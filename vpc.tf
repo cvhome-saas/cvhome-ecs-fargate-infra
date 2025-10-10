ff5c277908b3d5ce6debc6abcf61ba09ffe39210
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-10-10 21:11:56 +0300

    add proxy lambdas

--- vpc.tf
+++ vpc.tf
@@ -25,3 +25,67 @@ module "vpc" {
 
   tags = local.tags
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = module.vpc.private_route_table_ids
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-${var.env}-s3-endpoint"
+  })
+}
+
+
+resource "aws_security_group" "vpce" {
+  name        = "${var.project}-${var.env}-vpce-sg"
+  description = "Security group for VPC Interface Endpoints"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [local.vpc_cidr] # allow HTTPS from inside the VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+# ---------------------------------------------------------
+# 🧩 Interface Endpoints (ECS, ECR, CloudWatch, STS, ServiceDiscovery)
+# ---------------------------------------------------------
+locals {
+  interface_endpoints = {
+    ecr_dkr          = "com.amazonaws.${var.region}.ecr.dkr"
+    sts              = "com.amazonaws.${var.region}.sts"
+    ecr_api          = "com.amazonaws.${var.region}.ecr.api"
+    logs             = "com.amazonaws.${var.region}.logs"
+    servicediscovery = "com.amazonaws.${var.region}.servicediscovery"
+    data_servicediscovery = "com.amazonaws.${var.region}.data-servicediscovery"
+  }
+}
+
+resource "aws_vpc_endpoint" "interface" {
+  for_each = local.interface_endpoints
+
+  vpc_id              = module.vpc.vpc_id
+  service_name        = each.value
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.vpce.id]
+  private_dns_enabled = true
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-${var.env}-${each.key}-endpoint"
+  })
+}
