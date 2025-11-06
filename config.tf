d17e6b790b58f5f9e36cb76e4d22b55801b954ed
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-06 17:59:11 +0300

    extend pod configuration with auto-scaling and production flags

--- config.tf
+++ config.tf
@@ -8,4 +8,6 @@ locals {
   image_tag = nonsensitive(jsondecode(data.aws_ssm_parameter.config-cvhome.value).image_tag)
   pod_count = nonsensitive(jsondecode(data.aws_ssm_parameter.config-cvhome.value).pod_count)
   pod_size = nonsensitive(jsondecode(data.aws_ssm_parameter.config-cvhome.value).pod_size)
+  pod_auto_scale = nonsensitive(jsondecode(data.aws_ssm_parameter.config-cvhome.value).pod_auto_scale)
+  is_prod = nonsensitive(jsondecode(data.aws_ssm_parameter.config-cvhome.value).is_prod)
 }
